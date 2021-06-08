6e3b07bab69618718a3d892650b9ae7bf2e8b346
coco <ergal@163.com>
2021-06-08 18:43:59 +0800

    feat: add toc auto change toc anchor

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,68 @@ const chroma = function () {
 
 chroma();
 
+// toc
+const toc = {};
+
+const tocScroll = function () {
+  $(window).scroll(function () {
+    $("#TableOfContents a").removeClass("current");
+    currentAnchor().addClass("current");
+  });
+};
+
+const getAnchors = function () {
+  if (toc.anchors == null) {
+    var _anchors = $("#TableOfContents a").map(function () {
+      return $(this).attr("href");
+    });
+    toc.anchors = _anchors;
+  }
+  return toc.anchors;
+};
+
+const tocItem = function (anchor) {
+  return $('[href="' + anchor + '"]');
+}
+
+const heading = function (anchor) {
+  return $("[id=" + anchor.substr(1) + "]");
+}
+
+const currentAnchor = function () {
+  var winY = window.pageYOffset;
+  var currAnchor = null;
+  getAnchors().each(function () {
+    // var currentPos = winY;
+    // var anchorPos = heading(this).position().top;
+    var y = heading(this).position().top;
+    // console.log("heading y:" + y);
+    // console.log("winY y:" + winY);
+    // console.log("innerh:" + window.innerHeight);
+    // if (y < winY + window.innerHeight * 0.23) {
+    if (y < winY + 40) {
+      currAnchor = this;
+      return;
+    }
+  });
+  return tocItem(currAnchor);
+};
+
+const initFirstHeadingPosition = function () {
+  var _anchors = getAnchors();
+  toc.firstHeadTop = heading(_anchors[0]).position().top;
+}
+
+toc.anchors = null;
+toc.firstHeadTop = 0;
+
+toc.init = function () {
+  getAnchors();
+  initFirstHeadingPosition();
+  tocScroll();
+};
+
 $(document).ready(function () {
   header.init();
+  toc.init();
 });
